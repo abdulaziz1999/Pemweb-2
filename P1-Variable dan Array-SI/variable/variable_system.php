405ae0017a0eb48855288f5fa261206fa9877556
abdulaziz1999 <azizmentor96@gmail.com>
2022-03-02 18:06:54 +0700

    pertemuan kedua form

---
+++ P1-Variable dan Array-SI/variable/variable_system.php
@@ -0,0 +1,8 @@
+<?php 
+
+    //variable system
+    echo '<h3>Variable System</h3>';
+    echo 'Dokument Root : '.$_SERVER['DOCUMENT_ROOT'].'<br>';
+    echo 'Nama File : '.$_SERVER['PHP_SELF'].'<br>';
+
+?>
